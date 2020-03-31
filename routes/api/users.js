fe989be1f45b522e61bb603606f2e089f1fa2979
piter20005 <piterst25@gmail.com>
2020-03-31 22:46:06 +0200

    Rules of validating for registration and login

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 
 // Load Input Validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // Load User model
 const User = require("../../models/User");
@@ -74,6 +75,16 @@ router.post("/register", (req, res) => {
 // @desc   Login User / Returning JWT Token
 // @access Public
 router.post("/login", (req, res) => {
+    const {
+        errors,
+        isValid
+    } = validateLoginInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     const email = req.body.email;
     const password = req.body.password;
 
@@ -83,9 +94,8 @@ router.post("/login", (req, res) => {
     }).then(user => {
         // Check for user
         if (!user) {
-            return res.status(404).json({
-                email: "User not found"
-            });
+            errors.email = 'Users not found'
+            return res.status(404).json(errors);
         }
 
         // Check Password
@@ -112,9 +122,8 @@ router.post("/login", (req, res) => {
                         });
                     });
             } else {
-                return res.status(400).json({
-                    password: "Password incorrect"
-                });
+                errors.password = 'Password incorrect'
+                return res.status(400).json(errors);
             }
         });
     });
