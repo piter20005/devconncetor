54e800c3db59682777920429b981a8501c98aecb
piter20005 <piterst25@gmail.com>
2020-03-31 14:08:08 +0200

    Adding JWT token to user when login

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://adminPiter:dupacycki@cluster0-y76a2.mongodb.net/test?retryWrites=true&w=majority'
-}
+    mongoURI: 'mongodb+srv://adminPiter:dupacycki@cluster0-y76a2.mongodb.net/test?retryWrites=true&w=majority',
+    secretOrKey: 'secret'
+};
